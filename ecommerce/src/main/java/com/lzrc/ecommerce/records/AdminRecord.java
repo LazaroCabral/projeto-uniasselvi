2944e0a470434d0963c8e9d03e0949e78eeb7f49
Lazaro Cabral <lazarocabralamro@gmail.com>
2025-06-20 01:08:22 -0300

    validacoes para admins adicionadas

--- ecommerce/src/main/java/com/lzrc/ecommerce/records/AdminRecord.java
+++ ecommerce/src/main/java/com/lzrc/ecommerce/records/AdminRecord.java
@@ -1,5 +1,18 @@
 package com.lzrc.ecommerce.records;
 
-public record AdminRecord(String cpf, String name, String password) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record AdminRecord(
+    
+    @NotBlank(message = "O cpf não pode ser nulo!")
+    @Size(max = 11, min = 11, message = "O cpf deve ter 11 caracteres!")
+    String cpf,
+    @NotBlank(message = "O nome não pode ser nulo!")
+    @Size(max = 60, message = "O nome não pode ser maior que 60 caracteres!")
+    String name,
+    @NotBlank(message = "O senha não pode ser nula!")
+    @Size(max = 60, message = "O senha não pode ser maior que 60 caracteres!")
+    String password) {
 
 }
