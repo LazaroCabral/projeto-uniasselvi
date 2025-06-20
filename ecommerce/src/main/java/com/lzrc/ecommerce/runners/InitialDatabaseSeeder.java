8aa16d71a66bb84476ef12120900c15d6b89ab57
Lazaro Cabral <lazarocabralamro@gmail.com>
2025-06-20 01:01:14 -0300

    seeder para o admin padrao

---
+++ ecommerce/src/main/java/com/lzrc/ecommerce/runners/InitialDatabaseSeeder.java
@@ -0,0 +1,25 @@
+package com.lzrc.ecommerce.runners;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.lzrc.ecommerce.db.entities.Admin;
+import com.lzrc.ecommerce.db.repositories.AdminRepository;
+
+@Component
+public class InitialDatabaseSeeder implements CommandLineRunner {
+
+    @Autowired
+    AdminRepository adminRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Long hasAdmin = adminRepository.count();
+        if(hasAdmin == 0){
+            Admin admin = new Admin("00000000000", "admin", "admin1234");
+            adminRepository.save(admin);
+        }
+    }
+
+}
