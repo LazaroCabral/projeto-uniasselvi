0133a08beadeea72be4f59b5fc26f3348bab37eb
Lazaro Cabral <lazarocabralamro@gmail.com>
2025-06-20 01:04:56 -0300

    todos os argumentos no construtor

--- ecommerce/src/main/java/com/lzrc/ecommerce/db/entities/Admin.java
+++ ecommerce/src/main/java/com/lzrc/ecommerce/db/entities/Admin.java
@@ -3,13 +3,18 @@ package com.lzrc.ecommerce.db.entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 
 @Entity
 @Table(name = "admins")
+@AllArgsConstructor
 public class Admin {
 
     @Id
     private String cpf;
     private String name;
     private String password;
+
+    public Admin(){}
+
 }
