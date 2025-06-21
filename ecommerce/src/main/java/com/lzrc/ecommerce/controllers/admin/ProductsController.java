6fa9e1625ca7e236ef340d8b38be12f59642f7d8
Lazaro Cabral <lazarocabralamro@gmail.com>
2025-06-21 14:28:46 -0300

    feat: adicao de produtos com imagem

--- ecommerce/src/main/java/com/lzrc/ecommerce/controllers/admin/ProductsController.java
+++ ecommerce/src/main/java/com/lzrc/ecommerce/controllers/admin/ProductsController.java
@@ -1,17 +1,24 @@
 package com.lzrc.ecommerce.controllers.admin;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.lzrc.ecommerce.db.entities.Product;
 import com.lzrc.ecommerce.db.repositories.ProductRepository;
 import com.lzrc.ecommerce.records.ProductRecord;
+import com.lzrc.ecommerce.services.product.ProductService;
+import com.lzrc.ecommerce.services.product.exceptions.SaveImageException;
 
 import jakarta.validation.Valid;
 
@@ -22,21 +29,39 @@ public class ProductsController {
     @Autowired
     ProductRepository productRepository;
 
+    @Autowired
+    ProductService productService;
+
     @GetMapping("/add-product")
     public String addProduct(){
         return "admin/products/add-product.html";
     }
 
-    @PostMapping("/add-product")
+    @PostMapping(path = "/add-product", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Transactional
-    public ModelAndView addProductPost(@Valid ProductRecord productRecord, BindingResult bindingResult){
+    public ModelAndView addProductPost(@RequestParam("product-image") MultipartFile productImage,
+            @Valid ProductRecord productRecord, BindingResult bindingResult) throws IOException{
+                
         ModelAndView mv = new ModelAndView("admin/products/add-product.html");
         if(bindingResult.hasErrors()){
             return mv;
         }
         Product product = new Product(productRecord.sku(), productRecord.name(), 
             productRecord.description(), productRecord.price());
-        productRepository.save(product);
+        if(productImage.isEmpty()){
+            productService.save(product);
+            mv.addObject("success", Boolean.valueOf(true));
+            return mv;
+        }
+        try {
+            productService.saveWithImage(productImage.getBytes(), product);
+            productService.save(product);
+            mv.addObject("success", Boolean.TRUE);
+        } catch (SaveImageException e) {
+            e.printStackTrace();
+            mv.addObject("imageIsSaved", Boolean.FALSE);
+            productService.save(product);
+        }
         mv.addObject("success", Boolean.valueOf(true));
         return mv;
     }
