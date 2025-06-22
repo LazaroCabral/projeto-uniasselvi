acd686e7b3e71259345e6796366158daca0f32ef
Lazaro Cabral <lazarocabralamro@gmail.com>
2025-06-22 20:17:51 -0300

    endpoint para a visualizacao do produto

--- ecommerce/src/main/java/com/lzrc/ecommerce/controllers/ProductsController.java
+++ ecommerce/src/main/java/com/lzrc/ecommerce/controllers/ProductsController.java
@@ -1,5 +1,36 @@
 package com.lzrc.ecommerce.controllers;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.lzrc.ecommerce.db.entities.Product;
+import com.lzrc.ecommerce.db.repositories.ProductRepository;
+import com.lzrc.ecommerce.records.ProductRecord;
+
+@Controller("public-products-controller")
 public class ProductsController {
 
+    @Autowired
+    ProductRepository productRepository;
+
+    @GetMapping("/product/{id}")
+    public ModelAndView product(@PathVariable("id") String productId){
+        ModelAndView mv = new ModelAndView("product.html");
+        Optional<Product> productOptinal=productRepository.findById(productId);
+        if(productOptinal.isPresent()){
+            Product product = productOptinal.get();
+            ProductRecord productRecordResponse = new ProductRecord(
+                product.getSku(), product.getName(), product.getDescription()
+                , product.getPrice());
+            mv.addObject("product", productRecordResponse);
+        } else {
+            mv.addObject("productIsFound", Boolean.FALSE);
+        }
+        return mv;
+    }
 }
