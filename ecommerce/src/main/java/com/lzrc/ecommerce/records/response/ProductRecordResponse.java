e8e573d70cacba3f3afb3867e2cf22135fdb605c
Lazaro Cabral <lazarocabralamro@gmail.com>
2025-06-22 17:08:28 -0300

    record de produto para resposta

---
+++ ecommerce/src/main/java/com/lzrc/ecommerce/records/response/ProductRecordResponse.java
@@ -0,0 +1,11 @@
+package com.lzrc.ecommerce.records.response;
+
+import java.math.BigDecimal;
+
+public record ProductRecordResponse(
+    String sku,
+    String name,
+    String description,
+    BigDecimal price) {
+
+}
