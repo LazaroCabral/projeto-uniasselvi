1e5833f3c7e93ac33a3a2672aa5613df66b45313
Lazaro Cabral <lazarocabralamro@gmail.com>
2025-06-22 21:59:35 -0300

    feat: endpoint para a listagem de produtos

--- ecommerce/src/main/java/com/lzrc/ecommerce/controllers/ProductsController.java
+++ ecommerce/src/main/java/com/lzrc/ecommerce/controllers/ProductsController.java
@@ -3,6 +3,8 @@ package com.lzrc.ecommerce.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,12 +13,22 @@ import org.springframework.web.servlet.ModelAndView;
 import com.lzrc.ecommerce.db.entities.Product;
 import com.lzrc.ecommerce.db.repositories.ProductRepository;
 import com.lzrc.ecommerce.records.ProductRecord;
+import com.lzrc.ecommerce.records.response.ProductRecordResponse;
 
 @Controller("public-products-controller")
 public class ProductsController {
 
     @Autowired
     ProductRepository productRepository;
+
+    @GetMapping("/products")
+    public ModelAndView home(Pageable pageable){
+        ModelAndView mv = new ModelAndView("products.html");
+        Page<ProductRecordResponse> products = productRepository
+            .findAllBy(pageable);
+        mv.addObject("products", products.getContent());
+        return mv;
+    }
 
     @GetMapping("/product/{id}")
     public ModelAndView product(@PathVariable("id") String productId){
