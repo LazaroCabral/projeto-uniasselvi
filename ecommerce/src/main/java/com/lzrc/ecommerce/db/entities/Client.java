95b79bf4bda6f1507229bacc0af6489303410cef
Lazaro Cabral <lazarocabralamro@gmail.com>
2025-06-23 15:13:45 -0300

    construtor com todos os campos, entidade client

--- ecommerce/src/main/java/com/lzrc/ecommerce/db/entities/Client.java
+++ ecommerce/src/main/java/com/lzrc/ecommerce/db/entities/Client.java
@@ -5,11 +5,13 @@ import java.math.BigDecimal;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Entity
 @Table(name = "clients")
 @Getter
+@AllArgsConstructor
 public class Client {
 
     @Id
@@ -18,4 +20,6 @@ public class Client {
     private String email;
     private String password;
     private BigDecimal accountBalance;
+
+    public Client(){}
 }
