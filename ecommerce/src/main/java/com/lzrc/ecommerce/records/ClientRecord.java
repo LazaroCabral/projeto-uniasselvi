8e4722edf4f137f84804e85f423a75ed361c17c1
Lazaro Cabral <lazarocabralamro@gmail.com>
2025-06-23 17:36:03 -0300

    validacao de campos do ClientRecord

--- ecommerce/src/main/java/com/lzrc/ecommerce/records/ClientRecord.java
+++ ecommerce/src/main/java/com/lzrc/ecommerce/records/ClientRecord.java
@@ -2,7 +2,27 @@ package com.lzrc.ecommerce.records;
 
 import java.math.BigDecimal;
 
-public record ClientRecord(String cpf,String name,String email,
-    String password,BigDecimal accountBalance) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record ClientRecord(
+    
+    @NotBlank(message = "O cpf não pode ser nulo!")
+    @Size(min = 11,max = 11, message = "O cpf deve possuir 11 numeros!")
+    String cpf,
+
+    @NotBlank(message = "O nome não pode ser nulo!")
+    @Size(max = 60, message = "O nome não pode ser maior que 60 caracteres!")
+    String name,
+
+    @NotBlank(message = "O email não pode ser nulo!")
+    @Size(max = 40, message = "O email não pode ser maior que 4 caracteres!")
+    String email,
+
+    @NotBlank(message = "A senha não pode ser nula!")
+    @Size(max = 30, message = "A senha não pode ser maior que 30 caracteres!")
+    String password,
+
+    BigDecimal accountBalance) {
 
 }
