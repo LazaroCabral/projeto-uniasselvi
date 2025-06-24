b92e6b97f1f437a755cfc6064b8ee97929136cf3
Lazaro Cabral <lazarocabralamro@gmail.com>
2025-06-24 16:29:16 -0300

    controller de login de clientes

---
+++ ecommerce/src/main/java/com/lzrc/ecommerce/controllers/client/LoginController.java
@@ -0,0 +1,16 @@
+package com.lzrc.ecommerce.controllers.client;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/client")
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "clients/login.html";
+    }
+
+}
