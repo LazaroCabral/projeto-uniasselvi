4a0d38bbe98e05938d03d7609d99bdeb212007b2
Lazaro Cabral <lazarocabralamro@gmail.com>
2025-06-24 15:34:17 -0300

    controller para login de admins

---
+++ ecommerce/src/main/java/com/lzrc/ecommerce/controllers/admin/LoginController.java
@@ -0,0 +1,16 @@
+package com.lzrc.ecommerce.controllers.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller("admin-login-controller")
+@RequestMapping("/admin")
+public class LoginController {
+
+    @GetMapping("/login")
+    public String adminLogin(){
+        return "admin/login.html";
+    }
+    
+}
