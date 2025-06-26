a8ae9f0a40cf5530746d8cf3df12960e42d6a58e
Lazaro Cabral <lazarocabralamro@gmail.com>
2025-06-26 18:14:40 -0300

    classe usada para produtos pegos

---
+++ ecommerce/src/main/java/com/lzrc/ecommerce/services/product/purchase/HeldProduct.java
@@ -0,0 +1,14 @@
+package com.lzrc.ecommerce.services.product.purchase;
+
+import com.lzrc.ecommerce.db.entities.Product;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class HeldProduct {
+
+    Product product;
+
+}
