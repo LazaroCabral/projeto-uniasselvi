0c5cdcb2dc51fc6b2e38d0ecb8441c9d9858f1d6
Lazaro Cabral <lazarocabralamro@gmail.com>
2025-06-29 14:56:55 -0300

    pegar produto com write lock

--- ecommerce/src/main/java/com/lzrc/ecommerce/db/repositories/custom/CustomProductRepository.java
+++ ecommerce/src/main/java/com/lzrc/ecommerce/db/repositories/custom/CustomProductRepository.java
@@ -1,16 +1,24 @@
 package com.lzrc.ecommerce.db.repositories.custom;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.lzrc.ecommerce.db.entities.Product;
 import com.lzrc.ecommerce.records.response.ProductRecordResponse;
 
+import jakarta.persistence.LockModeType;
+
 @Repository
 public interface CustomProductRepository extends CrudRepository<Product,String>{
 
      Page<ProductRecordResponse> findAll(Pageable pageable);
+
+     @Lock(LockModeType.PESSIMISTIC_WRITE)
+     Optional<Product> findById(String sku);
 
 }
