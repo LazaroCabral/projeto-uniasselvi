39525bcfce688501beb2a188138606d5045bc00e
Lazaro Cabral <lazarocabralamro@gmail.com>
2025-07-03 15:16:01 -0300

    comando seeder admin por meio do service

--- ecommerce/src/main/java/com/lzrc/ecommerce/runners/InitialDatabaseSeeder.java
+++ ecommerce/src/main/java/com/lzrc/ecommerce/runners/InitialDatabaseSeeder.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 
 import com.lzrc.ecommerce.db.entities.Admin;
 import com.lzrc.ecommerce.db.repositories.AdminRepository;
+import com.lzrc.ecommerce.services.admin.AdminService;
 
 @Component
 public class InitialDatabaseSeeder implements CommandLineRunner {
@@ -13,12 +14,15 @@ public class InitialDatabaseSeeder implements CommandLineRunner {
     @Autowired
     AdminRepository adminRepository;
 
+    @Autowired
+    AdminService adminService;
+
     @Override
     public void run(String... args) throws Exception {
         Long hasAdmin = adminRepository.count();
         if(hasAdmin == 0){
-            Admin admin = new Admin("00000000000", "admin", "{noop}admin1234");
-            adminRepository.save(admin);
+            Admin admin = new Admin("00000000000", "admin", "admin1234");
+            adminService.update(admin);
         }
     }
 
